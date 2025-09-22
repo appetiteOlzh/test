adff80dcdde8d8ad3ad53258fa2ec6bc9c4cc232
Olzhas Zhussipbek <olzh.zhussipbek@yandex.kz>
2025-09-22 14:17:29 +0500

    test dvh on mob device

--- src/app/(page)/application/page.tsx
+++ src/app/(page)/application/page.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/widgets/footer";
 
 export default function ApplicationPage() {
   return (
-    <main className="min-h-full sm:flex sm:flex-col h-[100dvh] sm:h-auto">
+    <main className="min-h-full flex flex-col h-[100dvh] sm:h-auto">
       <Header withDownload deskOnly />
       <div className="flex-1 flex items-center sm:py-20 min-h-0">
         <Wrapper>
