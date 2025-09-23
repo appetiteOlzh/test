55bc4b85b55340f5b894c3fcb4bfde56761a8b16
Olzhas Zhussipbek <olzh.zhussipbek@yandex.kz>
2025-09-23 14:33:50 +0500

    metadata updated for application page

--- src/app/(page)/application/page.tsx
+++ src/app/(page)/application/page.tsx
@@ -2,6 +2,36 @@ import { Header } from "@/widgets/header";
 import { Wrapper } from "./ui";
 import { VerticalScroll } from "./ui/swiper";
 import { Footer } from "@/widgets/footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://monclips.com"),
+  title: "MonClips Gallery — Your camera roll, but like TTok.",
+  description:
+    "Every time you scroll, MonClips shows you a random photo or video from your gallery. It’s your life — surprising, emotional, unforgettable.",
+  openGraph: {
+    type: "website",
+    siteName: "Monclips",
+    images: [
+      {
+        url: "/public/assets/img/og-bg.png", // Must be an absolute url
+      },
+    ],
+    description:
+      "Every time you scroll, MonClips shows you a random photo or video from your gallery. It’s your life — surprising, emotional, unforgettable.",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@monclips",
+    creator: "@olzh_zh",
+    images: "/public/assets/img/og-bg.png",
+    description:
+      "Every time you scroll, MonClips shows you a random photo or video from your gallery. It’s your life — surprising, emotional, unforgettable.",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function ApplicationPage() {
   return (
