085e49fa4d38f5544335a4432f7b999203e954c2
Olzhas Zhussipbek <olzh.zhussipbek@yandex.kz>
2026-04-25 10:31:30 +0500

    meta added and imgs tinified

--- src/app/(page)/organize/page.tsx
+++ src/app/(page)/organize/page.tsx
@@ -10,6 +10,56 @@ import {
   Blockquote,
   Download,
 } from "./ui";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MonClips — Новый инструмент для ведущих и организаторов",
+  description:
+    "Альбом после мероприятия по одной ссылке - новый формат для тех, кто хочет больше зарабатывать на своих мероприятиях",
+  authors: [{ name: "MonClips" }],
+  creator: "MonClips",
+  publisher: "MonClips",
+  metadataBase: new URL("https://monclips.com"),
+  alternates: {
+    canonical: "/organize",
+  },
+  openGraph: {
+    title: "MonClips — Новый инструмент для ведущих и организаторов",
+    description:
+      "Альбом после мероприятия по одной ссылке - новый формат для тех, кто хочет больше зарабатывать на своих мероприятиях",
+    url: "https://monclips.com/organize",
+    siteName: "MonClips",
+    images: [
+      {
+        url: "/og_img_organize.png",
+        width: 1200,
+        height: 630,
+        alt: "MonClips — Cloud storage for photos and videos",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MonClips — Новый инструмент для ведущих и организаторов",
+    description:
+      "Альбом после мероприятия по одной ссылке - новый формат для тех, кто хочет больше зарабатывать на своих мероприятиях",
+    images: ["/og_img_organize.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  category: "technology",
+};
 
 export default function Page() {
   return (
